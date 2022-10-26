ca637742e2788a7711ddb86751c3d43ab00ffcde
alex1779 <alex1779@gmail.com>
2022-10-26 12:10:00 -0300

    Adding new sections on index page

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
 const menuhamb = document.querySelector('#icon-button-button');
 menuhamb.onclick = function () { hamburgClick(); };
 document.querySelector('#ico').onclick = function () { initialExec(); };
+
+function hamburgClick() {
+  alert('click');
+}
